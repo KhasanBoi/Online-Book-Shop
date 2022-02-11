bca3b5298b3beca577d86a9e676c878d8656b681
khasanboi <k.kholmatov@student.inha.uz>
2022-02-11 18:29:58 +0500

    Add project files.

---
+++ Online-Book-Shop/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Online_Book_Shop.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
